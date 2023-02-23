5d1198ef7a28cdbd28b98cb66fa9cb9fdda2af61
themuku <murad.mesteliyev3@gmail.com>
2023-02-23 21:31:56 +0400

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 30;
+export const RES_PER_PAGE = 10;
+export const KEY = 'd1472c68-6b30-4d1e-8b47-a96974992f0c';
+export const MODAL_CLOSE_SEC = 2.5;
